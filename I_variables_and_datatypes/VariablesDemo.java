b455205bdd3c384a087498efeb7e82040697a2e9
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2025-02-04 22:17:57 +0530

    Create VariablesDemo.java

---
+++ I_variables_and_datatypes/VariablesDemo.java
@@ -0,0 +1,12 @@
+package I_variables_and_datatypes;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int a; // declaration
+        a = 11; // assignment
+
+        int b = 1; // declare and assign variable
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
