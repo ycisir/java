0674241c560f4977fc993ec3287ffaac3b5394fc
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2025-02-04 22:17:29 +0530

    Create DataTypesDemo.java

---
+++ I_variables_and_datatypes/DataTypesDemo.java
@@ -0,0 +1,35 @@
+package I_variables_and_datatypes;
+
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int a=1, b=99;
+        System.out.println("the value of a: "+a);
+        System.out.println("the value of b: "+b);
+
+        byte by = 125;
+        System.out.println(by);
+        short sh = 3535;
+        System.out.println(sh);
+
+        long lg = 212322334324L; // literals needed   F/f
+        System.out.println(lg);
+
+        float price = 15.5F;
+        System.out.println(price);
+
+        double dl = 321323D; // literals needed   D/d
+        System.out.println(dl);
+
+
+        char grade = 'A';
+        System.out.println(grade);
+
+        String name = "harry";
+        System.out.println(name);
+
+        boolean tr = true;
+        System.out.println(tr);
+
+
+    }
+}
