90b6eacb071155e84866ccb986d72e2d12882a27
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2025-02-04 22:22:42 +0530

    Create LoopsDemo.java

---
+++ IV_loops/LoopsDemo.java
@@ -0,0 +1,117 @@
+package IV_loops;
+
+public class LoopsDemo {
+    public static void main(String[] args) {
+
+//        1: while
+//        print 1..10
+//        int i=1; // initialization
+//        while (i <= 10) { // condition
+//            System.out.println(i);
+//            i++; // increment
+//        }
+
+
+//        print event numbers
+//        int i=2;
+//        while (i <= 10) {
+//            System.out.println(i);
+//            i+=2;
+//        }
+
+//        approach 2
+//        int i=1;
+//        while (i <= 10) {
+//            if (i%2 == 0)
+//                System.out.println(i);
+//            i++;
+//        }
+
+
+//        int i=1;
+//        while (i <= 10) {
+//            if (i%2 == 0)
+//                System.out.println(i+" even");
+//            else
+//                System.out.println(i+" odd");
+//            i++;
+//        }
+
+//        descending order
+//        int i=10;
+//        while (i > 0) {
+//            System.out.println(i);
+//            i--;
+//        }
+
+
+
+
+
+//        2: do while
+//        statement will execute atleast once
+
+//        print 1..10
+//        int i=1;
+//        do {
+//            System.out.println(i);
+//            i++;
+//        } while (i <= 10);
+
+//        descending
+//        int i=10;
+//        do {
+//            System.out.println(i);
+//            i--;
+//        } while (i > 0);
+
+
+//        diff b/w while and do while
+//        int i=10;
+//        while (i <= 5) {
+//            System.out.println(i); // not execute bcz condition is false
+//            i++;
+//        }
+
+//        int i=10;
+//        do {
+//            System.out.println(i); // execute alteast once. o/p => 10
+//            i++;
+//        } while (i <= 5);
+
+
+
+
+//        2: for loop
+        //  init only first time, then check condition after that print statement and
+        // lastly increment or decrement
+//        print 1..10
+//        for (int i=0; i<=10; i++) {
+//            System.out.println(i);
+//        }
+
+//        print even number
+//        for (int i=2; i<=10; i+=2) {
+//            System.out.println(i);
+//        }
+
+//        for (int i=1; i<=10; i++) {
+//            if (i%2 == 0)
+//                System.out.println(i+" even");
+//            else
+//                System.out.println(i+" odd");
+//        }
+
+
+//        print 10..1
+//        for (int i=10; i>0; i--) {
+//            System.out.println(i);
+//        }
+
+
+//        which loop is preferred to use
+//        1. if you know the number of iteration in advance use for loop
+//        2. if we want to execute the loop atleast one do-while is appropriate
+//        3. if we are uncertain about the number of iteration and the loop termination is based on the condition then while is good choice
+    }
+}
