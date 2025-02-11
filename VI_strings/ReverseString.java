2760b112f2c6bf08c9640960c5c2a4bef3d04858
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2025-02-11 22:18:56 +0530

    Create ReverseString.java

---
+++ VI_strings/ReverseString.java
@@ -0,0 +1,40 @@
+package VI_strings;
+
+public class ReverseString {
+    public static void main(String[] args) {
+//        String s = "welcome";
+
+//        approach1: length(), charAt()
+//        System.out.println("original string: "+s);
+//        String rev = "";
+//        for (int i = s.length()-1; i >= 0 ; i--) {
+//            rev += s.charAt(i);
+//        }
+//        System.out.println("reverse string: "+rev);
+
+
+
+//        approach2: convert to character array
+//        System.out.println("original string: "+s);
+//        String rev = "";
+//        char arr[] = s.toCharArray();
+//        for (int i = arr.length-1; i >= 0 ; i--) {
+//            rev += arr[i];
+//        }
+//        System.out.println("reverse string: "+rev);
+
+
+
+//        approach3: string buffer
+//        StringBuffer s = "something"; // cant declare like this in stringbuffer
+//        StringBuffer s = new StringBuffer("something");
+//        System.out.println(s.reverse()); // direct reverse method
+
+
+
+//        approach4: string builder
+//        StringBuilder s = new StringBuilder("builder");
+//        System.out.println(s.reverse());
+
+    }
+}
